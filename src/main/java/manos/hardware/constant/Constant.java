1a1f9842c277ed5f8a013dc7ecf6b1c29afe47a0
ArthurPeracolli <arthur.freitas@sptech.school>
2022-10-12 18:46:16 -0300

    Create insets to table constantHardware and dynamicHardware.

--- src/main/java/manos/hardware/constant/Constant.java
+++ src/main/java/manos/hardware/constant/Constant.java
@@ -1,27 +1,42 @@
 package manos.hardware.constant;
 
 import com.github.britooo.looca.api.core.Looca;
+import manos.connection.database.DatabaseConfig;
+import manos.hardware.conversor.Conversor;
 
 public class Constant {
-    Long diskSize;
-    String diskModel;
-    String ramSize;
-    String nameProcessor;
-    Integer cores;
-    String systemOperational;
-    Integer threads;
+    private Long diskSize;
+    private String diskModel;
+    private String ramSize;
+    private String cpuName;
+    private Integer cpuCore;
+    private String operationalSystem;
 
     public Constant() {
         Looca looca = new Looca();
 
         this.diskSize = looca.getGrupoDeDiscos().getTamanhoTotal();
-        this.diskModel = diskModel;
-        this.systemOperational = looca.getSistema().getSistemaOperacional();
-        this.ramSize = String.format("%.1f", looca.getMemoria().getTotal() / Math.pow(1024, 3));
-        this.nameProcessor = looca.getProcessador().getNome();
-        this.cores = looca.getProcessador().getNumeroCpusFisicas();
-        this.threads = looca.getProcessador().getNumeroCpusLogicas();
+        this.diskModel = looca.getGrupoDeDiscos().getDiscos().get(0).getNome();
+        this.operationalSystem = looca.getSistema().getSistemaOperacional();
+        this.ramSize = Conversor.formatarBytes(looca.getMemoria().getTotal());
+        this.cpuName = looca.getProcessador().getNome();
+        this.cpuCore = looca.getProcessador().getNumeroCpusFisicas();
     }
 
+    public static void constantData() {
+        DatabaseConfig connection = new DatabaseConfig();
+        Constant constant = new Constant();
 
+        String update = String.format("INSERT INTO constantHardware VALUES (null, %s, %d, %s, %s, %l, %s, null)",
+                constant.cpuName,
+                constant.cpuCore,
+                constant.ramSize,
+                constant.diskModel,
+                constant.diskSize,
+                constant.operationalSystem
+        );
+
+        connection.getConnection().update(update);
+
+    }
 }
