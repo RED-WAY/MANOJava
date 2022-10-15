f14df2da8138d78212c266ff857d1d48482434fd
VitorMendesco <vitor.mcosta@sptech.school>
2022-10-15 15:12:34 -0300

    build: disk test

--- src/main/java/manos/views/App.java
+++ src/main/java/manos/views/App.java
@@ -6,17 +6,17 @@ public class App {
 
     public static void main(String[] args) {
         Looca looca = new Looca();
-        System.out.println(looca.getGrupoDeDiscos().getDiscos().get(0).getModelo());
+        System.out.println(looca.getGrupoDeDiscos().getDiscos().get(0));
 
-        View view = new View();
-
-        try {
-            Thread.sleep(1000);
-            view.setVisible(true);
-            view.startupApp();
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
+//        View view = new View();
+//
+//        try {
+//            Thread.sleep(1000);
+//            view.setVisible(true);
+//            view.startupApp();
+//        } catch (InterruptedException ex) {
+//            ex.printStackTrace();
+//        }
 
     }
 }
