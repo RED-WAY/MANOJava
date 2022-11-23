5de57746aa677720db51fdff069f2c62f76bcf7a
PedroFreitas <pedro.freitas@sptech.school>
2022-11-23 19:09:53 -0300

    updatede- object class Operation

---
+++ src/main/java/manos/process/Operation.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manos.process;
+
+/**
+ *
+ * @author pedro
+ */
+public class Operation {
+    
+      private Integer idOperation;
+      private String operationName; 
+      private String operationType; 
+
+    public Integer getIdOperation() {
+        return idOperation;
+    }
+
+    public void setIdOperation(Integer idOperation) {
+        this.idOperation = idOperation;
+    }
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(String operationType) {
+        this.operationType = operationType;
+    }
+      
+      
+    
+}
